4c09482f62c9834945f1aea3ba22e5c9905a2ca5
David Turner <76234472+MonsterInTheCreek@users.noreply.github.com>
2024-12-03 23:09:49 -0500

    Add Day 3

--- README.md
+++ README.md
@@ -2,3 +2,7 @@
 
 Participating with Ball State University CS Graduate Program students.
 Each of us have our own public repos, not working cooperatively.
+
+## Commit history
+
+Why two initial commits?  It's git, don't ask.  I'm aware that it looks weird.  :)
--- day03.py
+++ day03.py
@@ -1,5 +1,10 @@
-# Day X AoC 2024
-# Using inputXX.txt data
+# Day 3 AoC 2024
+# Using input03.txt data
+# Input data is list of 6 long strings of variable length
+# initial pattern == mul(n,n)
+
+
+import re
 
 
 def get_data(file):
@@ -7,15 +12,70 @@ def get_data(file):
         return source.readlines()
 
 
-#def process()
-#more logic
-#def solve_a()
-#def solve_b()
+def preprocess(dataset):
+    '''
+    This feels kludgy, but should work.
+    Use arbitrary Unicode symbols to replace do() and don't().
+    Then loop through strings 'recording' the proper passages as preprocess.
+    '''
+    rec = "\u23fa"
+    stop = "\u23f9"
+    filtered = []
+    record = True  # originally had this inside for loop, reset state each line
+    for long_string in dataset:
+        long_string = long_string.replace("do()", rec)
+        long_string = long_string.replace("don't()", stop)
+        temp_string = ""
+        for let in long_string:
+            if let == rec:
+                record = True           # and go to next letter
+            elif let == stop:
+                record = False          # and go to next letter
+            elif record == True:
+                temp_string += let
+        filtered.append(temp_string)    # maintain list of strings
+    return filtered
 
 
-# change input
+def process(dataset):
+    all_muls = []
+    for long_string in dataset:
+        regex_pattern = r"mul\((\d+),(\d+)\)"
+        mul_list = re.findall(regex_pattern, long_string)
+        all_muls += mul_list
+    return all_muls
+    
+
+def solve_a(all_muls):
+    total = 0
+    for mul in all_muls:
+        total += int(mul[0]) * int(mul[1])
+    print(f"Day 3, first = {total}")
+
+
+def solve_b(all_muls):
+    total = 0
+    for mul in all_muls:
+        total += int(mul[0]) * int(mul[1])
+    print(f"Day 3, second = {total}")
+
+
 if __name__ == "__main__":   
-    input = get_data("inputXX.txt")
-    reports = process(input)
-    solve_a(reports)
-    solve_b(reports)
+    input_a = get_data("input03.txt")
+    input_b = input_a.copy()
+    muls_a = process(input_a)
+    solve_a(muls_a)
+    muls_b = preprocess(input_b)
+    muls_b = process(muls_b)
+    solve_b(muls_b)
+
+
+## Original logic of part 2 I barfed the regex.  Then figured out correct regex.
+## But still got wrong answer.
+## After testing each list item individually with many debugs,
+## I still couldn't see where it was wrong.  I had to look at Reddit.
+## I thought state was resetting on each new list item.
+## A bunch of people on Reddit with same issue.
+## But the directions do specifically say that mul is enabled at beginning of
+## program, not beginning of each new list item.  My mistake.
+
